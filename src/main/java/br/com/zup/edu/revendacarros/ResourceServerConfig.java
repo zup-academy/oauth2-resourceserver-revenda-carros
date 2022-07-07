84ed620b5012c3310cf4e635377d55c2a651e369
Rafael Ponte <rafael.ponte@zup.com.br>
2022-07-07 11:05:42 -0300

    Spring Security: algumas boas práticas;

--- src/main/java/br/com/zup/edu/revendacarros/ResourceServerConfig.java
+++ src/main/java/br/com/zup/edu/revendacarros/ResourceServerConfig.java
@@ -5,22 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http
-            .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/carros").hasAuthority("SCOPE_carros:read")
-                .antMatchers(HttpMethod.POST, "/api/carros").hasAuthority("SCOPE_carros:write")
-            .anyRequest()
-                .authenticated()
+        // @formatter:off
+        http.cors()
             .and()
-                .oauth2ResourceServer()
-                    .jwt()
-                    ;
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().disable()
+                .rememberMe().disable()
+                .httpBasic().disable()
+                .requestCache().disable()
+                .headers().frameOptions().deny()
+            .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(STATELESS)
+            .and()
+                .authorizeRequests()
+                    .antMatchers(GET, "/api/carros").hasAuthority("SCOPE_carros:read")
+                    .antMatchers(POST, "/api/carros").hasAuthority("SCOPE_carros:write")
+                .anyRequest()
+                    .authenticated()
+                .and()
+                    .oauth2ResourceServer()
+                        .jwt()
+        ;
+        // @formatter:on
     }
 }
